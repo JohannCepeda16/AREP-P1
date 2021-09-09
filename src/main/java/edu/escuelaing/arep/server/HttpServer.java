39e4d1ab083e03365c46367da06e262403698be5
Johann Cepeda <johann.cepeda@mail.escuelaing.edu.co>
2021-09-09 09:42:15 -0500

    Project structure

---
+++ src/main/java/edu/escuelaing/arep/server/HttpServer.java
@@ -0,0 +1,78 @@
+package edu.escuelaing.arep.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import edu.escuelaing.arep.calculator.Calculator;
+
+/**
+ * Calculadora de seno, coseno y tangente
+ * @author Johann Cepeda
+ */
+public class HttpServer {
+
+    /**
+     * Metodo principal de la clase
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(35000);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 35000.");
+            System.exit(1);
+        }
+        Socket clientSocket = null;
+        try {
+            clientSocket = serverSocket.accept();
+        } catch (IOException e) {
+            System.err.println("Accept failed.");
+            System.exit(1);
+        }
+
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        String inputLine, outputLine;
+        String currentOp = "cos";
+
+        while ((inputLine = in.readLine()) != null) {
+            if (inputLine.startsWith("fun")) {
+                String[] op = inputLine.split(":");
+                if (!op[1].equals("sen") && !op[1].equals("cos") && !op[1].equals("tan")) {
+                    out.println("Operacion no valida");
+                } else {
+                    currentOp = op[1];
+                    System.out.println("Cambio de operacion a: " + currentOp);
+                    out.println("Cambio de operacion a: " + currentOp);
+                }
+            } else {
+                try {
+                    if (currentOp.equals("cos"))
+                        outputLine = Calculator.cos(inputLine);
+                    else if (currentOp.equals("sen"))
+                        outputLine = Calculator.sen(inputLine);
+                    else if (currentOp.equals("tan"))
+                        outputLine = Calculator.tan(inputLine);
+                    else
+                        outputLine = "Operacion invalida";
+
+                    // Resultados
+                    out.println(outputLine);
+                } catch (Exception e) {
+                    throw new Exception("Formato invalido");
+                }
+            }
+        }
+
+        out.close();
+        in.close();
+        clientSocket.close();
+        serverSocket.close();
+    }
+}
